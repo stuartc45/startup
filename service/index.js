4f5a6ba035240ad9f9b36d18a8fa9dd8b3278ad0
Stuart Card <stuartcard450@gmail.com>
2025-11-05 07:05:52 -0700

    Added the setup code to index.js that sets up express, the cookie parser, and the apiRouter

--- service/index.js
+++ service/index.js
@@ -1,2 +1,20 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
+
+let users = [];
+let entries = [];
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
