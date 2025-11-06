e41f31d793cdfeb0c0153e6b06516724beac9ddf
Stuart Card <stuartcard450@gmail.com>
2025-11-05 22:35:19 -0700

    Added some functionality in the backend for login and register user

--- service/index.js
+++ service/index.js
@@ -20,12 +20,28 @@ app.use(`/api`, apiRouter);
 
 // Registers a user
 apiRouter.post('/auth/register', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createUser(req.body.email, req.body.password);
 
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+    }
 });
 
 // logs in a user
 apiRouter.post('/auth/login', async (req, res) => {
-
+    const user = await findUser('email', req.body.email);
+      if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+          user.token = uuid.v4();
+          setAuthCookie(res, user.token);
+          res.send({ email: user.email });
+          return;
+        }
+      }
+      res.status(401).send({ msg: 'Unauthorized' });
 });
 
 // logs out a user
@@ -37,3 +53,32 @@ apiRouter.delete('/auth/logout', async (req, res) => {
 apiRouter.post('/entry', async (req, res) => {
 
 });
+
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
