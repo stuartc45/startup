5ad5310ebd6f35e04a5dbb7647ce20ab739bb346
Stuart Card <stuartcard450@gmail.com>
2025-11-13 00:39:47 -0700

    Added the database.js file and the functionality of adding users to the database

---
+++ service/database.js
@@ -0,0 +1,43 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const entryCollection = db.collection('entry');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+};
--- service/index.js
+++ service/index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = 'token';
 
@@ -12,8 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
-let users = [];
-let entries = [];
+// let users = [];
+// let entries = [];
 
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
@@ -36,6 +37,7 @@ apiRouter.post('/auth/login', async (req, res) => {
       if (user) {
         if (await bcrypt.compare(req.body.password, user.password)) {
           user.token = uuid.v4();
+          await DB.updateUser(user);
           setAuthCookie(res, user.token);
           res.send({ email: user.email });
           return;
@@ -50,6 +52,7 @@ apiRouter.delete('/auth/logout', async (req, res) => {
     const user = await findUser('token', req.cookies[authCookieName]);
     if (user) {
         delete user.token;
+        DB.updateUser(user);
     }
     res.clearCookie(authCookieName);
     res.status(204).end();
@@ -144,7 +147,8 @@ async function createUser(email, password) {
         password: passwordHash,
         token: uuid.v4(),
     };
-    users.push(user);
+    await DB.addUser(user);
+    // users.push(user);
 
     return user;
 }
@@ -153,7 +157,12 @@ async function createUser(email, password) {
 async function findUser(field, value) {
     if (!value) return null;
 
-    return users.find((u) => u[field] === value);
+    if (field === 'token') {
+        return DB.getUserByToken(value);
+      }
+    return DB.getUser(value);
+
+    // return users.find((u) => u[field] === value);
 }
 
 
